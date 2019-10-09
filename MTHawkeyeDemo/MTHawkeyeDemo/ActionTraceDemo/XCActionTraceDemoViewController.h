4d0cdfc6a23be3fb474c7fbb0e7d81846451a36d
Tong Leiming <tongleiming@daling.com>
2019-10-09 18:39:34 +0800

    在Demo中添加增加行为埋点的测试ViewController

--- MTHawkeyeDemo/MTHawkeyeDemo/ActionTraceDemo/XCActionTraceDemoViewController.h
+++ MTHawkeyeDemo/MTHawkeyeDemo/ActionTraceDemo/XCActionTraceDemoViewController.h
@@ -14,7 +14,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface XCActionTraceViewController : UIViewController
+@interface XCActionTraceDemoViewController : UIViewController
 
 @end
 
--- MTHawkeyeDemo/MTHawkeyeDemo/ActionTraceDemo/XCActionTraceDemoViewController.m
+++ MTHawkeyeDemo/MTHawkeyeDemo/ActionTraceDemo/XCActionTraceDemoViewController.m
@@ -10,24 +10,42 @@
 //
 
 
-#import "XCActionTraceViewController.h"
+#import "XCActionTraceDemoViewController.h"
 #import "XCActionTrace.h"
 
-@interface XCActionTraceViewController ()
+@interface XCActionTraceDemoViewController ()
+
+@property (nonatomic, strong) UITextView *textView;
+
+@property (nonatomic, strong) NSMutableString *log;
 
 @end
 
-@implementation XCActionTraceViewController
+@implementation XCActionTraceDemoViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    self.textView = [[UITextView alloc] init];
+    self.textView.editable = NO;
+    self.view = self.textView;
+    
+    self.log = [NSMutableString string];
+    
     self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addActionTrace)];
 }
 
 - (void)addActionTrace {
+    static NSInteger index = 0;
+    NSString *title = [NSString stringWithFormat:@"actionTitle%ld", (long)index++];
+    NSDictionary *params = @{@"params1":@"hello", @"params2":@"world"};
+    [[XCActionTrace sharedInstance] addActionWithTitle:title params:params];
     
+    [self.log appendFormat:@">> %@ \n", title];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        self.textView.text = self.log;
+    });
 }
 
 /*
